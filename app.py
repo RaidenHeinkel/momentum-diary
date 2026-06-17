1a18fb6b26647089cd60614436ee0dc822ed5e9c
RaidenHeinkel <37789027+RaidenHeinkel@users.noreply.github.com>
2026-06-17 12:44:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import datetime
 
 # 設定：GASのURLとスプレッドシートの公開CSV URL
-GAS_URL = "https://script.google.com/macros/s/AKfycbwRPt8wGjYF7TAyuYoFNzBultUamJm4RFMKymL_zvvFxA2mRKFLwRKL1uoUTqyuZNbbdA/exec"
+GAS_URL = "https://script.google.com/macros/s/AKfycbwMd1q0uXDMbjtGCS3hhoo9JY8r-iNAApMXkTBMUqeFQ9LstUGbbsvlYk-Rh9IIWOR_dQ/exec"
 SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT6UXrWkViMBqVFvkayaXQ76oKeh47-hDe9rbqOEFMAlppFcu-KDrz-MMsPUKFIGcHmjrkT8MmrT7SX/pub?output=csv"
 
 st.set_page_config(page_title="Momentum Diary", layout="centered")
